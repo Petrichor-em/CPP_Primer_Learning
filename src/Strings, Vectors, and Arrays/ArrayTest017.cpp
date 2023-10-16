7ad3f5d1b6e224d82a98f8328e8e2df83e0f5916
Petrichor-em <jinyi_liu@foxmail.com>
2023-10-16 23:58:22 +0800

    Mon Oct 16 23:58:22     2023

--- src/Strings, Vectors, and Arrays/ArrayTest017.cpp
+++ src/Strings, Vectors, and Arrays/ArrayTest017.cpp
@@ -7,7 +7,7 @@ int main(){
 	//类型别名简化多维数组的指针
 	typedef int int_array[4]; //int_array是四个元素的数组类型
 	for(int_array *p = ia; p!=end(ia); ++p){ //int_array *p 表明p是一个指针,
-											//这个指针指向有四个元素的数组类型的变量, 即该数组的名字
+		//这个指针指向有四个元素的数组类型的变量, 即该数组的名字
 		for(int *q = *p; q!=end(*p); ++q){
 			cout << *q << " ";
 		}
