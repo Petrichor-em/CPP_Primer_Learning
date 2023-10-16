6a1c7ede2ef81826dea29238ebfbba3d1b341bb6
Petrichor-em <jinyi_liu@foxmail.com>
2023-10-16 23:51:10 +0800

    Mon Oct 16 23:51:10     2023

---
+++ src/Strings, Vectors, and Arrays/ArrayTest017.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iterator>
+using namespace std;
+
+int main(){
+	int ia[3][4] = {{1},{2},{3}};
+	//类型别名简化多维数组的指针
+	typedef int int_array[4]; //int_array是四个元素的数组类型
+	for(int_array *p = ia; p!=end(ia); ++p){ //int_array *p 表明p是一个指针,
+											//这个指针指向有四个元素的数组类型的变量, 即该数组的名字
+		for(int *q = *p; q!=end(*p); ++q){
+			cout << *q << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+	return 0;
+}
